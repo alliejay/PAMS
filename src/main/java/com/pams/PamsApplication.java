b42ed3a0917a8e16114d2a90b544a547ff8fb801
cpoakley1432 <cpoakley1432@gmail.com>
2015-12-08 12:49:22 -0500

    initial import

---
+++ src/main/java/com/pams/PamsApplication.java
@@ -0,0 +1,12 @@
+package com.pams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PamsApplication.class, args);
+    }
+}
